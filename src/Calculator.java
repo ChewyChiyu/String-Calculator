71a5fbcdecc065a56d80e1e5bc429856131cb5f5
EvanC <ecchen83@gmail.com>
2018-10-06 23:16:58 -0700

    Revamp whole class

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,187 +1,137 @@
 
+import java.util.ArrayList;
 import java.util.Stack;
 
-public class Calculator {
+public class Calculator{
 
+	private final String[] symbols = new String[]{"-","+","/","*","^","sin","cos","tan","(",")"};
 
-	Stack<Double>outputStack = new Stack<Double>();
-	Stack<Symbol> inputStack = new Stack<Symbol>();
-	String[] equArr;
-
-
-	Symbol[] SYMBOLS;
-
-	public static void main(String[] args){
-		new Calculator().calculate("((3)+3/3-3)");
+	public void calculate(String func){
+		compute(parse(func));
 	}
 
-	public Calculator(){
-		loadSymbols();
-	}
-
-	private void loadSymbols(){
-		SYMBOLS = new Symbol[]{
-				new Symbol("-",1),
-				new Symbol("+",1),
-				new Symbol("/",2),
-				new Symbol("*",2),
-				new Symbol("^",3),
-				new Symbol("sin",4),
-				new Symbol("cos",4),
-				new Symbol("tan",4),
-				new Symbol("(",-1),
-				new Symbol(")",-2),
-		};
-	}
-
-	public void calculate(String equation){
-		outputStack.clear();
-		inputStack.clear();
-
-		parse(equation);
-
-		System.out.println(compute());
-
-	}
-
-	public double compute(){
-
-		for(String s : equArr){
-			if(s.matches("-?\\d+(\\.\\d+)?")){ // numeric
-				outputStack.push(Double.parseDouble(s));
-			}else{
-				Symbol sym = Symbol.getSymbol(SYMBOLS, s);
-				if(sym.value>0){ // no special operation
-					if(!inputStack.isEmpty() && (inputStack.peek().compareTo(sym) < 0)){ 
-						while(!inputStack.isEmpty() && inputStack.peek().value > 0){
-							step();
-						}
-					}
-					inputStack.push(sym);
-				}else{ // special parentheses backtrack 
-					if(sym.value==-1){
-						inputStack.push(sym);
-					}else{
-						while(inputStack.peek().value!=-1){
-							step();
-						}
-						inputStack.pop();
+	public void compute(ArrayList<String> func){
+		Stack<String> input = new Stack<String>();
+		Stack<Double> output = new Stack<Double>();
+		boolean compress = false;
+		String prev = func.get(0);
+		for(String symbol : func){
+			if(!compress)
+				compress = ((isNumber(prev)||prev.equals(")"))&&symbol.equals("("));
+			prev = symbol;
+			if(isNumber(symbol)){
+				output.add(Double.parseDouble(symbol));
+			}else if(symbol.equals("(")){
+				input.add(symbol);
+			}else if(symbol.equals(")")){
+				while(!input.peek().equals("(")){
+					step(input,output);
+				}
+				input.pop();
+				if(compress){
+					printStack(output);
+					printStack(input);
+					System.out.println("test");
+					compress = false;
+					if(output.size()>1){
+						output.add(output.pop()*output.pop());
 					}
 				}
-
+				
+			}else{
+				while(!input.isEmpty()&&!isHigher(symbol,input.peek())){
+					step(input,output);
+				}
+				input.add(symbol);
 			}
-
-		}
-
-		while(!inputStack.isEmpty()){
-			step();
-		}
-
-
-		return outputStack.pop();
+		}	
+		while(!input.isEmpty()||output.size()>1){ step(input,output); }
+		System.out.println(output.peek());
 	}
 
-	public void step(){
-		Symbol sym = inputStack.pop();
-		double d3 = 0;
-		if(sym.value < 4){
-			double d1 = outputStack.pop();
-			double d2 = outputStack.pop();
-			switch(sym.symbol){
-			case "+":
-				d3 = d2+d1;
-				break;
-			case "-":
-				d3 = d2-d1;
-				break;
-			case "/":
-				d3 = d2/d1;
-				break;
-			case "*":
-				d3 = d2*d1;
-				break;
-			case "^":
-				d3 = Math.pow(d2, d1);
-				break;
-			}
 
-		}else{
-			double d1 = outputStack.pop();
-			switch(sym.symbol){
-			case "sin":
-				d3 = Math.sin(d1);
+
+	public void printStack(Stack s){
+		Object[] arr = s.toArray();
+		for(Object d : arr){
+			System.out.print( d + " ");
+		} 
+		System.out.println();
+	}
+
+	public void step(Stack<String> input, Stack<Double> output){
+		printStack(output);
+		printStack(input);
+		double d1 = 0, d2 = 0;
+		if(input.isEmpty() && output.size() < 2){ return; }
+		else if(input.isEmpty()){
+			d1 = output.pop();
+			d2 = output.pop();
+			output.add(d1*d2);
+		}else if(input.peek().equals("sin")||input.peek().equals("cos")||input.peek().equals("tan")){
+			String symbol = input.pop();
+			switch(symbol){
+				case "sin": output.add(Math.sin(output.pop()));
 				break;
-			case "cos":
-				d3 = Math.cos(d1);
+				case "cos": output.add(Math.cos(output.pop()));
 				break;
-			case "tan":
-				d3 = Math.tan(d1);
+				case "tan": output.add(Math.tan(output.pop()));
 				break;
+				default: break;
 			}
 		}
-		outputStack.add(d3);
-	}
-	
-	public <T> void verbose(Stack<T> stack){
-		 Object[] d = stack.toArray();
-		 for(Object o : d){
-			 System.out.print(o);
-		 }
-		 System.out.println();
-	}
-	
-	public void parse(String equation){
-		String str = equation;
-		final String LIMITER = "~";
-
-		for(Symbol symbol : SYMBOLS){
-			str = str.replace(symbol.symbol, LIMITER + symbol.symbol + LIMITER);
-		}
-
-		equArr = str.split("~");
-
-		int count = 0;
-		for(String s : equArr){
-			if(!s.equals("")){
-				count++;
+		else{
+			String symbol = input.pop();
+			d1 = output.pop();
+			d2 = output.pop();
+			switch(symbol){
+				case "-": output.add(d2-d1);
+				break;
+				case "+": output.add(d2+d1);
+				break;
+				case "/": output.add(d2/d1);
+				break;
+				case "*": output.add(d2*d1);
+				break;
+				default: break;
 			}
 		}
-		String[] equAlpha = new String[count];
-		count = 0;
-		for(String s : equArr){
-			if(!s.equals("")){
-				equAlpha[count++] = s;
+	}
+
+	public boolean isHigher(String s1, String s2){
+		return getValue(s1) > getValue(s2);
+	}
+
+
+	public int getValue(String s1){
+		switch(s1){
+			case "-": case "+": return 1;
+			case "/": case "*": return 2;
+			case "^": return 3;
+			case "sin": case "cos": case "tan": return 4;
+			default: return -1;
+		}
+	}
+
+	public boolean isNumber(String s){
+		return(s.matches("-?\\d+(\\.\\d+)?"));  // numeric
+	}
+
+	public ArrayList<String> parse(String func){
+		for(String s : symbols){
+			 func = func.replace(s," "+s+" ");
+		}
+		String[] temp = func.split(" ");
+		ArrayList<String> arr = new ArrayList<String>(); 
+		for(String sym : temp){
+			if(!sym.trim().equals("")){
+				arr.add(sym);
 			}
 		}
-		equArr = equAlpha;
+		return arr;
 	}
 
-
-}
-
-class Symbol implements Comparable<Symbol>{
-
-	String symbol;
-	int value;
-
-	public Symbol(String symbol, int value){
-		this.symbol = symbol;
-		this.value = value;
+	public static void main(String[] args){
+		new Calculator().calculate("2(2(3)+10(1.5)+(3/4)+(1/4))");
 	}
-
-	public int compareTo(Symbol symbol) {
-		if(symbol.value>value){ return 1; }
-		else if(symbol.value<value){ return -1; }
-		return 0; //equal
-	}
-
-	public static Symbol getSymbol(Symbol[] arr, String s){
-		for(Symbol sym : arr){
-			if(sym.symbol.equals(s)){
-				return sym;
-			}
-		}
-		return null;
-	}
-
 }
